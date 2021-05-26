6a9e9afa922f961e04d72898595190ff0c86887e
MichalTyminski <mtyminski16@gmail.com>
2021-05-26 17:38:53 +0200

    Create Board_class.h

---
+++ Board_class.h
@@ -0,0 +1,54 @@
+#ifndef BOARD_CLASS_H
+#define BOARD_CLASS_H
+
+#include <C:/SFML-2.5.1/include/SFML/Window.hpp>
+#include <C:/SFML-2.5.1/include/SFML/Graphics.hpp>
+#include <C:/SFML-2.5.1/include/SFML/Graphics/Text.hpp>
+#include <ostream>
+#include <iostream>
+#include "SNAKE_class.h"
+
+class Snake;
+
+class Board
+{
+public:
+    Board();
+    virtual ~Board();
+
+    struct StructFruit
+    {
+        int x;
+        int y;
+    }apple;
+
+    struct StructRock
+    {
+        int x;
+        int y;
+    }rock;
+
+
+    void game();
+    void set_size();
+    void level_check();
+    void set_rock_position();
+    void apple__rock();
+
+    int size;
+    int level;
+    int window_size_x;
+    int window_size_y;
+    std::vector<int> position_of_rock_x;
+    std::vector<int> position_of_rock_y;
+    float time_to_delay;
+    float time_delay;
+    float show_rock_time;
+    float show_rock_time_variable;
+    Snake *snake;
+    bool level_change;
+
+private:
+
+};
+#endif // BOARD_CLASS_H
